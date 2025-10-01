736d4cd2bcdf81bcd34285670cee86dcfa3dfbec
NyanLHtoo <nyannxtoo@gmail.com>
2025-10-01 13:36:52 +0630

    js D2

---
+++ javascript/js/app.js
@@ -0,0 +1,14 @@
+// and &&
+// at least one false return false
+console.log(25 < 35 && 35 < 35);
+
+// or ||
+// at least one true return true
+console.log(25 < 35 || 35 < 35);
+
+// not !
+let num = 1 ;
+
+if (!num) { 
+console.log("not Operator");
+}
